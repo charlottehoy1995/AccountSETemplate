844e30ea66d94f9b8923c1068f1f6182209c28df
Admin <Admin@STUDENT12>
2019-01-30 09:54:51 +0000

    Increased overall coverage to 80%.

---
+++ src/main/java/com/qa/persistence/domain/App.java
@@ -0,0 +1,10 @@
+package com.qa.persistence.domain;
+
+public class App {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+	}
+	
+
+}
--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -1,40 +1,51 @@
 package com.qa.MapTests;
 
+
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.qa.persistence.domain.Account;
 import com.qa.persistence.repository.AccountMapRepository;
 
+
 public class AccountServiceTest {
+	
+	private  String MOCK_DATA_ARRAY_ONE = "{\"firstName\":\"John\",\"lastName\":\"Doe\",\"accountNumber\":1234}";
+	private  String MOCK_DATA_ARRAY_TWO = "{\"firstName\":\"Charlotte\",\"lastName\":\"Hoy\",\"accountNumber\":1256}";
+	
+	private AccountMapRepository repo;
 
 	@Before
 	public void setup() {
-	
+		repo = new AccountMapRepository();
 	}
 	
 	@Test
 	public void addAccountTest() {
-		
+		repo.createAccount(MOCK_DATA_ARRAY_ONE);
+		assertEquals(MOCK_DATA_ARRAY_ONE, repo.findAccount(1234L));
 	}
 	
+
 	@Test
 	public void add2AccountTest() {
-		
+		String reply = repo.createAccount(MOCK_DATA_ARRAY_TWO);
+		Assert.assertEquals("Created new account with first name: Charlotte, Last name: Hoy, Account Number: 1256", reply);
 	}
 
 	@Test
 	public void removeAccountTest() {
-		
+		String reply = repo.deleteAccount(1234L);
 	}
+	
 	
 	@Test
 	public void remove2AccountTest() {
 		
 	}
+
 	
 	@Test
 	public void remove2AccountTestAnd1ThatDoesntExist() {
